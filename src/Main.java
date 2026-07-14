6a52fb03fa9d6d45cfd28d0dbc5833b4870cb087
nutbit <arta2250065@gmail.com>
2026-07-14 20:07:39 +0300

    Calendar

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,31 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int st = sc.nextInt();
-        if (st >= 18 ) {
-        System.out.println("Доступ дозволено");}
-        else{
-        System.out.println("Доступ заборонено");}
+        int day = sc.nextInt();
+        switch (day) {
+            case 1:
+                System.out.println("Понеділок");
+                break;
+            case 2:
+                System.out.println("Вівторок");
+                break;
+            case 3:
+                System.out.println("Середа");
+                break;
+            case 4:
+                System.out.println("Четверг");
+                break;
+            case 5:
+                System.out.println("Пʼятниця");
+                break;
+            case 6:
+                System.out.println("Субота");
+                break;
+            case 7:
+                System.out.println("Неділя");
+                break;
+            default:
+                System.out.println("Баба не чує 9 чи 10");
+        }
     }
 }
