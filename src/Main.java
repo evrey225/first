9e35f2ad3d3852fe1f933832319f24ec2ea5a636
nutbit <arta2250065@gmail.com>
2026-07-14 13:18:13 +0300

    second

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int x = 42;
+        float y = 2;
+        double z = 1.1;
+        boolean b = true;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(b);
     }
 }
