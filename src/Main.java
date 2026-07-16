d89e487bad081bb37a8cd32e3d3f0b46e37477d9
nutbit <arta2250065@gmail.com>
2026-07-16 16:57:19 +0300

    dilennya_na_0

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
-import java.util.ArrayList;
 class Main {
     public static void main(String[] args) {
-        ArrayList<String> list = new ArrayList<>(3);
-        list.add("Brovary");
-        list.add("Kyiv");
-        list.add("Odessa");
         Scanner sc = new Scanner(System.in);
-        String cityadd = sc.next();
-        list.add(cityadd);
-        System.out.println(list);
+        int x = sc.nextInt();
+        int z = sc.nextInt();
+        try {
+            int res = x/z;
+            System.out.println(res);
+        }
+        catch (Exception e){
+            System.out.println("На нуль ділити не можна");
+        }
     }
 }
